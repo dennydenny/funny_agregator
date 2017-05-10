285be1655ca6cdb587cf325c3cdc033ef0ae8315
Denny <dennyhaliulin@gmail.com>
2017-05-10 22:27:09 +0400

    Поднял версию.

--- src/main/java/fa/common/Controller.java
+++ src/main/java/fa/common/Controller.java
@@ -22,7 +22,7 @@ import fa.rankprocessing.RankProcessingDBHelper;
 
 public class Controller {
 
-	private static final String _version = "1.1";
+	private static final String _version = "1.2";
 	private static final Logger LOG = LoggerFactory.getLogger(Controller.class);
 	private static DBHelper db = new DBHelper();
 	
